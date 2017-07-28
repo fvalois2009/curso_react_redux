abb2d219d6e857d60da49770c0aa4a76faaabbc0
Felix Valois <fvalois2009@gmail.com>
2017-07-27 22:33:02 -0300

    ...

--- ex/index.js
+++ ex/index.js
@@ -1,1 +1,11 @@
-require('./duvidaCruel')
+class Pessoa {
+    constructor(nome){
+        this.nome = nome
+    }
+    toString() {
+        return `Pessoa: ${this.nome}`
+    }
+}
+
+const pessoa = new Pessoa('Ghilherme')
+console.log(pessoa.toString())
